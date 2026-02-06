fc44334f4088eff8ef2a03f2fdd5d1c3bd1e3005
3nya <enyac1207@gmail.com>
2026-02-06 14:12:10 -0600

    small mistake that couldnt open pdfs

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "assets", "Procedures"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Procedures"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
